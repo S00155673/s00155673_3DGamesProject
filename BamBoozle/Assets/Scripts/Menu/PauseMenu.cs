a06cb08d4a79cbe0a5b691c254956d5ad63b395f
Jordan Davies <S00155673@mail.itsligo.ie>
2018-08-08 17:39:51 +0100

    Pause Menu Added
    Simple Pause Menu, with small animation.

---
+++ BamBoozle/Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool IsPasued = false;
+
+    public GameObject pauseMenuPanel;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsPasued)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        IsPasued = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        IsPasued = false;
+    }
+
+    public void MainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
